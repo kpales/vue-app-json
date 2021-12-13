a8e482ad753d0c9c2a5f48eba4b3af6052afb4cf
kpales <alespetrovacki@gmail.com>
2021-12-13 13:48:38 -0500

    Integrating vue router for navigation

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,30 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <Navigation />
-      <div class="col-9 d-flex flex-wrap">
-        <div v-for="user in users" v-bind:key="user.id" class="col-4">
-          <img src="https://fakeimg.pl/300/" alt="" />
-          <p>{{ user.name }}</p>
-          <p>{{ user.username }}</p>
-        </div>
-      </div>
-      <!-- <div v-for="(user, key) in users" :key="key">
-            {{user[0].name}}
-        </div> -->
-    </div>
-    <!-- <span>{{user.name}}</span>
-      <p>{{user.username}}</p>  -->
-    <!-- id: {{ user.id }}<br> -->
-    <!-- name: {{ user.name }} -->
-    <div class="row">
-      
-      <!-- <div class="user-details d-flex"> -->
-        
-      <!-- </div> -->
-    </div>
-    
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/users">Users</router-link>
   </div>
+  <router-view/>
 </template>
 
-<script>
-import Navigation from "./components/Navigation.vue";
-import axios from "axios";
-export default {
-  name: "App",
-  components: {
-    Navigation,
-  },
-  data() {
-    return {
-      users_api: "https://jsonplaceholder.typicode.com/users",
-      users: [],
-    };
-  },
-  // created() {
-  //   fetch(`${this.users_api}`)
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then(this.setUsers);
-  // },
-  // mounted () {
-  //   axios.get('https://jsonplaceholder.typicode.com/users').then(response => (this.user = response.data)).catch(error => console.log(error))
-  // },
-  mounted() {
-    axios
-      .get(this.users_api)
-      .then(({ data }) => {
-        this.users = data;
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  },
-  // methods: {
-  //   setUsers(results) {
-  //     this.user = results;
-  //   },
-  // },
-};
-</script>
-
-<style>
+<style lang="scss">
 #app {
-  /* font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px; */
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
---
+++ src/views/Users.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="container">
+  <div class="row">
+    <div class="col-12 d-flex flex-wrap">
+        <div v-for="user in users" v-bind:key="user.id" class="col-4">
+          <img src="https://fakeimg.pl/300/" alt="" />
+          <p>{{ user.name }}</p>
+          <p>{{ user.username }}</p>
+        </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "App",
+  data() {
+    return {
+      users_api: "https://jsonplaceholder.typicode.com/users",
+      users: [],
+    };
+  },
+  mounted() {
+    axios
+      .get(this.users_api)
+      .then(({ data }) => {
+        this.users = data;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  },
+};
+</script>
