0ddc13c259a2deb8d0096c1ffc8fae813f87d28e
kpales <alespetrovacki@gmail.com>
2021-12-13 10:32:00 -0500

    Integrated bootstrap , prepared custom component navigation,integrating api calls

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <Navigation />
+        <div v-for="(user, key) in users" :key="key">
+          <!-- <p>Image</p>
+          <p>{{ user.name }}</p>
+          <p>{{ user.username }}</p> -->
+            id: {{ user.id }}<br>
+name: {{ user.name }}
+
+{{users}}
+        </div>
+      <!-- <span>{{user.name}}</span>
+      <p>{{user.username}}</p>  -->
+        <!-- id: {{ user.id }}<br> -->
+        <!-- name: {{ user.name }} -->
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navigation from "./components/Navigation.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Navigation,
+  },
+  data() {
+    return {
+      users_api: "https://jsonplaceholder.typicode.com/users",
+      user: {},
+    };
+  },
+  created() {
+    fetch(`${this.users_api}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then(this.setUsers);
+  },
+  methods: {
+    setUsers(results) {
+      this.users = results;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="col-3">
+    <div class="side-navigation">
+      <ul>
+        <li>Menu 1</li>
+        <li>Menu 2</li>
+        <li>Menu 3</li>
+        <li>Menu 4</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.side-navigation{
+    li{
+        list-style: none;
+    }
+}
+</style>
