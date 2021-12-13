ab92f1b02f53edd543b5cfe7dc1d3a1cbb85f2ed
kpales <alespetrovacki@gmail.com>
2021-12-13 13:39:15 -0500

    User list get and set on page

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,34 @@
   <div class="container">
     <div class="row">
       <Navigation />
-        <div v-for="(user, key) in users" :key="key">
-          <!-- <p>Image</p>
+      <div class="col-9 d-flex flex-wrap">
+        <div v-for="user in users" v-bind:key="user.id" class="col-4">
+          <img src="https://fakeimg.pl/300/" alt="" />
           <p>{{ user.name }}</p>
-          <p>{{ user.username }}</p> -->
-            id: {{ user.id }}<br>
-name: {{ user.name }}
-
-{{users}}
+          <p>{{ user.username }}</p>
         </div>
-      <!-- <span>{{user.name}}</span>
-      <p>{{user.username}}</p>  -->
-        <!-- id: {{ user.id }}<br> -->
-        <!-- name: {{ user.name }} -->
+      </div>
+      <!-- <div v-for="(user, key) in users" :key="key">
+            {{user[0].name}}
+        </div> -->
     </div>
+    <!-- <span>{{user.name}}</span>
+      <p>{{user.username}}</p>  -->
+    <!-- id: {{ user.id }}<br> -->
+    <!-- name: {{ user.name }} -->
+    <div class="row">
+      
+      <!-- <div class="user-details d-flex"> -->
+        
+      <!-- </div> -->
+    </div>
+    
   </div>
 </template>
 
 <script>
 import Navigation from "./components/Navigation.vue";
+import axios from "axios";
 export default {
   name: "App",
   components: {
@@ -29,31 +38,44 @@ export default {
   data() {
     return {
       users_api: "https://jsonplaceholder.typicode.com/users",
-      user: {},
+      users: [],
     };
   },
-  created() {
-    fetch(`${this.users_api}`)
-      .then((res) => {
-        return res.json();
+  // created() {
+  //   fetch(`${this.users_api}`)
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then(this.setUsers);
+  // },
+  // mounted () {
+  //   axios.get('https://jsonplaceholder.typicode.com/users').then(response => (this.user = response.data)).catch(error => console.log(error))
+  // },
+  mounted() {
+    axios
+      .get(this.users_api)
+      .then(({ data }) => {
+        this.users = data;
       })
-      .then(this.setUsers);
+      .catch((e) => {
+        console.log(e);
+      });
   },
-  methods: {
-    setUsers(results) {
-      this.users = results;
-    },
-  },
+  // methods: {
+  //   setUsers(results) {
+  //     this.user = results;
+  //   },
+  // },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
