3a4ca22ce26d0dce1378a4efa674e4e94ab361fe
kpales <alespetrovacki@gmail.com>
2021-12-13 23:46:06 -0500

    Integrated components and passing data with props , getting album data based on user ID

---
+++ src/components/UsersDetail.vue
@@ -0,0 +1,59 @@
+<template>
+<a href="www.google.si">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://fakeimg.pl/300/" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">{{userFullName}}</h5>
+        <p class="card-text">{{userName}}</p>
+    <p class="card-text">{{userId}}</p>
+  </div>
+</div>
+  </a>
+  <p :v-bind="randomAlbumID()">{{randomAlbumID()}}</p>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  props:{
+    userName: String,
+    userId: Number,
+    userFullName : String
+  },
+  name: "Users",
+  data() {
+    return {
+//       users_api: "https://jsonplaceholder.typicode.com/users",
+//       users: [],
+      albums_endpoint: "https://jsonplaceholder.typicode.com/",
+      albums: [],
+//       photos_api: "https://jsonplaceholder.typicode.com/albums/1/photos",
+//       user_id: [],
+//       photos: []
+     };
+   },
+  mounted() {
+    axios
+      .get(`${this.albums_endpoint}users/${this.userId}/albums `)
+      .then(({ data }) => {
+        this.albums = data;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  },
+  methods: {
+    randomAlbumID(){
+        return this.albums[Math.floor(Math.random() * this.albums.length)];
+    }
+  }
+//   methods: {
+//     // randPhoto = () => photos[Math.floor(Math.random() * photos.length)]
+//   }
+};
+</script>
+
+<style>
+
+</style>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,36 +1,61 @@
 <template>
-<div class="container">
-  <div class="row">
-    <div class="col-12 d-flex flex-wrap">
-        <div v-for="user in users" v-bind:key="user.id" class="col-4">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 d-flex flex-wrap">
+        <div v-for="user in users" v-bind:key="user.id" class="col-3 my-2">
+          <UsersDetail
+            :user-id = user.id
+            :user-name = user.username
+            :user-fullName = user.name
+           />
+        </div>
+
+        <!-- <div v-for="user in users" v-bind:key="user.id" class="col-12 col-lg-4">
+          <a v-bind:href="'https://jsonplaceholder.typicode.com/users/' + user.id + '/albums'">
           <img src="https://fakeimg.pl/300/" alt="" />
           <p>{{ user.name }}</p>
           <p>{{ user.username }}</p>
-        </div>
+          </a>
+        </div> -->
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
+import UsersDetail from "@/components/UsersDetail.vue";
 import axios from "axios";
 export default {
-  name: "App",
+  name: "Users-view",
+  components: {
+    UsersDetail,
+  },
   data() {
     return {
-      users_api: "https://jsonplaceholder.typicode.com/users",
+      users_endpoint: "https://jsonplaceholder.typicode.com/users",
       users: [],
+      album_endpoint: "https://jsonplaceholder.typicode.com/albums",
+      album:[]
     };
   },
   mounted() {
     axios
-      .get(this.users_api)
+      .get(this.users_endpoint)
       .then(({ data }) => {
         this.users = data;
       })
       .catch((e) => {
         console.log(e);
       });
-  },
+          axios
+      .get(this.album_endpoint)
+      .then(({ data }) => {
+        this.album = data;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 };
+
 </script>
